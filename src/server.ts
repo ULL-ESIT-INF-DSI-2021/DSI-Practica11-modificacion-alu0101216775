9ea91bc4906cdac9fc59699552ff39754fc111be
alu0101216775 <alu0101216775@ull.edu.es>
2021-05-19 09:54:03 +0100

    Server initialized

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+
+class Server {
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.configure();
+        this.routes(); 
+    }
+
+    configure() {
+        // Database
+        const DATABASE = 'mongodb://localhost/users';
+        mongoose.set('useFindAndModify', true);
+        mongoose.connect(process.env.MONGODB_URL || DATABASE, {
+            useNewUrlParser: true,
+            useCreateIndex: true
+        }).then(db => console.log("Database connected!"))
+        .catch(db => console.error("Error connecting to Database"));
+        //Port
+        this.app.set('port', process.env.PORT  || 3000);
+        //Middlewares
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: false}));
+    }
+
+    routes() {
+        this.app.use(apiRoutes);
+    }
+
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server listening on port', this.app.get('port'));
+        })
+    }
+}
+
+const server = new Server();
+server.start();
